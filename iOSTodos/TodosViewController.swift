99bde7b8d22d996b7fedb392208ecf4b56ec83e8
danimal141 <hideaki.ishii1204@gmail.com>
2015-02-15 18:50:47 +0900

    Implement to retrieve information from SQLite using NSFetchedResultsController

--- iOSTodos/TodosViewController.swift
+++ iOSTodos/TodosViewController.swift
@@ -7,22 +7,36 @@
 //
 
 import UIKit
+import CoreData
 
-class TodosViewController: UITableViewController {
+class TodosViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
+    var fetchedResultController: NSFetchedResultsController = NSFetchedResultsController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        fetchedResultController = self.getFetchedResultController()
+        fetchedResultController.delegate = self
+        fetchedResultController.performFetch(nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getFetchedResultController() -> NSFetchedResultsController {
+        fetchedResultController = NSFetchedResultsController(fetchRequest: self.todoFetchRequest(), managedObjectContext: managedObjectContext!, sectionNameKeyPath: nil, cacheName: nil)
+        return fetchedResultController
+    }
+    
+    func todoFetchRequest() -> NSFetchRequest {
+        let fetchRequest = NSFetchRequest(entityName: "Todos")
+        let sortDescriptor = NSSortDescriptor(key: "content", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        return fetchRequest
     }
 
     // MARK: - Table view data source
