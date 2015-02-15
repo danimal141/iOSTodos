9daf010c1b64137c4e10fab6b985a1dfd6ce496d
danimal141 <hideaki.ishii1204@gmail.com>
2015-02-15 16:02:19 +0900

    Initial Commit

---
+++ iOSTodosTests/iOSTodosTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSTodosTests.swift
+//  iOSTodosTests
+//
+//  Created by Hideaki Ishii on 2/15/15.
+//  Copyright (c) 2015 danimal141. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOSTodosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
